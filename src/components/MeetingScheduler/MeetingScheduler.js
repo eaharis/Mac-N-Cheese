3d9228b05aefcd205879b95cc4826d606367bf45
Jack Buckley <bucklj4@mcmaster.ca>
2020-11-14 11:33:26 -0500

    Added friend list to schedule page.

--- src/components/MeetingScheduler/MeetingScheduler.js
+++ src/components/MeetingScheduler/MeetingScheduler.js
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import {
-    teal, indigo, red, orange, blue, green
+    teal, indigo, red, orange, green
 } from '@material-ui/core/colors';
 import Paper from '@material-ui/core/Paper';
 import { ViewState, GroupingState, IntegratedGrouping, Resources } from '@devexpress/dx-react-scheduler';
 import {
     Scheduler,
-    MonthView,
     Toolbar,
     DateNavigator,
     Appointments,
@@ -16,6 +15,7 @@ import {
 } from '@devexpress/dx-react-scheduler-material-ui';
 import Schedule from "./Schedule";
 import "./MeetingScheduler.css"
+import { Container, Card, Row, Col, ListGroup, DropdownButton, Form } from 'react-bootstrap';
 
 
 const MeetingScheduler = () => {
@@ -80,32 +80,68 @@ const MeetingScheduler = () => {
     ];
 
     return (
-        <Paper>
-            {/* TODO: Don't allow dates in the past to be picked */}
-            <Scheduler data={Schedule} min={new Date(2020, 11, 3)} startDate="2020-11-12" endDate="2020-11-13">
-                <ViewState
-                    // Today
-                    defaultCurrentDate={new Date()}
-                    startDate="2020-11-12"
-                    endDate="2020-11-13"
-                />
-                <DayView startDayHour={9} />
-                <GroupingState grouping={[
-                    { resourceName: "macId" }
-                ]} />
-                <Appointments />
-                <Resources data={[{
-                    fieldName: 'macId',
-                    instances: owners
-                }]} />
-                <IntegratedGrouping />
-                <GroupingPanel />
-                <Toolbar />
-                <DateNavigator min="2020-08-12" />
-                <TodayButton />
+        <Container className="Profile">
+            <Row>
+                <Col md={4} className="mb-3">
+                    <Card>
+                        <Card.Body className="card-body">
+                            <h4>1. Choose your friends</h4>
+                            <Card className="mt-3 h-75">
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item className="d-flex justify-content-between align-items-center flex-wrap">
+                                        <Form.Control variant="text" placeholder="Search friends..." />
+                                    </ListGroup.Item>
+                                    <div className="scrollableFriendlist">
+                                        <ListGroup.Item active={true} className="d-flex justify-content-between align-items-center flex-wrap">
+                                            <h6 className="mb-0"><img src="https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQ4MTUwOTQyMDE1OTU2Nzk4/tom-holland-photo-jason-kempin-getty-images-801510482-profile.jpg" className="friendPhoto rounded-circle mr-2" />Esam Haris</h6>
+                                        </ListGroup.Item>
+                                        <ListGroup.Item className="d-flex justify-content-between align-items-center flex-wrap">
+                                            <h6 className="mb-0"><img src="https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQ4MTUwOTQyMDE1OTU2Nzk4/tom-holland-photo-jason-kempin-getty-images-801510482-profile.jpg" className="friendPhoto rounded-circle mr-2" />Esam Haris</h6>                                        </ListGroup.Item>
+                                        <ListGroup.Item className="d-flex justify-content-between align-items-center flex-wrap">
+                                            <h6 className="mb-0"><img src="https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQ4MTUwOTQyMDE1OTU2Nzk4/tom-holland-photo-jason-kempin-getty-images-801510482-profile.jpg" className="friendPhoto rounded-circle mr-2" />Esam Haris</h6>                                        </ListGroup.Item>
+                                        <ListGroup.Item className="d-flex justify-content-between align-items-center flex-wrap">
+                                            <h6 className="mb-0"><img src="https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQ4MTUwOTQyMDE1OTU2Nzk4/tom-holland-photo-jason-kempin-getty-images-801510482-profile.jpg" className="friendPhoto rounded-circle mr-2" />Esam Haris</h6>                                        </ListGroup.Item>
+                                        <ListGroup.Item className="d-flex justify-content-between align-items-center flex-wrap">
+                                            <h6 className="mb-0"><img src="https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQ4MTUwOTQyMDE1OTU2Nzk4/tom-holland-photo-jason-kempin-getty-images-801510482-profile.jpg" className="friendPhoto rounded-circle mr-2" />Esam Haris</h6>                                        </ListGroup.Item>
+                                    </div>
+                                </ListGroup>
 
-            </Scheduler>
-        </Paper>
+
+                            </Card>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={8}>
+                    <Paper>
+                        {/* TODO: Don't allow dates in the past to be picked */}
+                        <Scheduler data={Schedule} min={new Date(2020, 11, 3)} startDate="2020-11-12" endDate="2020-11-13">
+                            <ViewState
+                                // Today
+                                defaultCurrentDate={new Date()}
+                                startDate="2020-11-12"
+                                endDate="2020-11-13"
+                            />
+                            <DayView startDayHour={9} />
+                            <GroupingState grouping={[
+                                { resourceName: "macId" }
+                            ]} />
+                            <Appointments />
+                            <Resources data={[{
+                                fieldName: 'macId',
+                                instances: owners
+                            }]} />
+                            <IntegratedGrouping />
+                            <GroupingPanel />
+                            <Toolbar />
+                            <DateNavigator min="2020-08-12" />
+                            <TodayButton />
+
+                        </Scheduler>
+                    </Paper>
+                </Col>
+            </Row>
+        </Container >
+
     );
 }
 
