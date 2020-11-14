c039e5791d3abb7a284f204b4c4b9af191ca9d01
Ali <howidia@mcmaster.ca>
2020-11-13 19:49:51 -0500

    just adding small basics

--- src/components/Promotions.js
+++ src/components/Promotions.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class promotions extends Component {
-
+    state = {}
+    render() {
+        return(<div className={"bg-info pb-3"}>
+            <h1 className={"display-1 text-light"}>Live Feed</h1>
+        </div>)
+    }
 }
 
 export default connect()(promotions);
