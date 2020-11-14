cebc085aabe2ce1ea683f7a1ba2702b2a1c23a2a
Jack Buckley <bucklj4@mcmaster.ca>
2020-11-14 11:58:29 -0500

    Using a list to generate the friends in MeetingScheduler.

--- src/components/MeetingScheduler/MeetingScheduler.js
+++ src/components/MeetingScheduler/MeetingScheduler.js
@@ -47,6 +47,57 @@ const MeetingScheduler = () => {
         }
     ];
 
+    const friendList = [
+        {
+            name: "Declan Wu",
+            macId: "wu92",
+            image: "https://www.famousbirthdays.com/faces/bell-drake-image.jpg",
+            active: true
+        },
+        {
+            name: "Tobey Maguire",
+            macId: "tobey",
+            image: "https://vz.cnwimg.com/thumb-1200x/wp-content/uploads/2009/11/GettyImages-461562263-e1579796582726.jpg",
+            active: false
+        },
+        {
+            name: "Esam Haris",
+            macId: "harise",
+            image: "https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQ4MTUwOTQyMDE1OTU2Nzk4/tom-holland-photo-jason-kempin-getty-images-801510482-profile.jpg",
+            active: true
+        },
+        {
+            name: "Ali Howidi",
+            macId: "howidia",
+            image: "https://pbs.twimg.com/profile_images/1045049020794761231/9EXXIIk6.jpg",
+            active: true
+        },
+        {
+            name: "Jake Johnson",
+            macId: "jake",
+            image: "https://www.earwolf.com/wp-content/uploads/2013/11/7750-300x300.jpg",
+            active: false
+        },
+        {
+            name: "Alan Turing",
+            macId: "turing",
+            image: "https://i.cbc.ca/1.2900956.1421864100!/fileImage/httpImage/image.jpg_gen/derivatives/original_620/alan-turing.jpg",
+            active: false
+        },
+        {
+            name: "Alonzo Church",
+            macId: "church",
+            image: "https://galois.com/wp-content/uploads/2014/06/Church1-480x480.jpeg",
+            active: false
+        },
+        {
+            name: "David Parnas",
+            macId: "parnas",
+            image: "https://memberservices.membee.com/feeds/membershipclientfiles/1498/images/a68e8450-8523-5341-ce09-116cb4fd582b.jpg",
+            active: false
+        },
+    ];
+
     return (
         <Container className="Profile">
             <Row>
@@ -60,17 +111,11 @@ const MeetingScheduler = () => {
                                         <Form.Control variant="text" placeholder="Search friends..." />
                                     </ListGroup.Item>
                                     <div className="scrollableFriendlist">
-                                        <ListGroup.Item active={true} className="d-flex justify-content-between align-items-center flex-wrap">
-                                            <h6 className="mb-0"><img src="https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQ4MTUwOTQyMDE1OTU2Nzk4/tom-holland-photo-jason-kempin-getty-images-801510482-profile.jpg" className="friendPhoto rounded-circle mr-2" />Esam Haris</h6>
-                                        </ListGroup.Item>
-                                        <ListGroup.Item className="d-flex justify-content-between align-items-center flex-wrap">
-                                            <h6 className="mb-0"><img src="https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQ4MTUwOTQyMDE1OTU2Nzk4/tom-holland-photo-jason-kempin-getty-images-801510482-profile.jpg" className="friendPhoto rounded-circle mr-2" />Esam Haris</h6>                                        </ListGroup.Item>
-                                        <ListGroup.Item className="d-flex justify-content-between align-items-center flex-wrap">
-                                            <h6 className="mb-0"><img src="https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQ4MTUwOTQyMDE1OTU2Nzk4/tom-holland-photo-jason-kempin-getty-images-801510482-profile.jpg" className="friendPhoto rounded-circle mr-2" />Esam Haris</h6>                                        </ListGroup.Item>
-                                        <ListGroup.Item className="d-flex justify-content-between align-items-center flex-wrap">
-                                            <h6 className="mb-0"><img src="https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQ4MTUwOTQyMDE1OTU2Nzk4/tom-holland-photo-jason-kempin-getty-images-801510482-profile.jpg" className="friendPhoto rounded-circle mr-2" />Esam Haris</h6>                                        </ListGroup.Item>
-                                        <ListGroup.Item className="d-flex justify-content-between align-items-center flex-wrap">
-                                            <h6 className="mb-0"><img src="https://www.biography.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTQ4MTUwOTQyMDE1OTU2Nzk4/tom-holland-photo-jason-kempin-getty-images-801510482-profile.jpg" className="friendPhoto rounded-circle mr-2" />Esam Haris</h6>                                        </ListGroup.Item>
+                                        {friendList.map(friend =>
+                                            <ListGroup.Item key={friend.macId} active={friend.active} className="d-flex justify-content-between align-items-center flex-wrap">
+                                                <h6 className="mb-0 font-weight-bold"><img src={friend.image} className="friendPhoto rounded-circle mr-2" />{friend.name}</h6>
+                                            </ListGroup.Item>
+                                        )}
                                     </div>
                                 </ListGroup>
 
